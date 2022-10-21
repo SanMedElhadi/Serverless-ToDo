aa8c571b4c67c0c215bc452b04c6cfb5061dbdd4
Mohammed Elhadi SANNEF <fm_SANNEF@esi.dz>
2022-10-21 18:39:27 +0000

    Update createTodo.ts

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -30,6 +30,7 @@ export const handler = middy(
       statusCode: 201,
       headers: {
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify({
         item : item
